c91c53450dca1cda4d1d367d7fb70c8c5c7b7054
Ertuğrul Güler <ertugrul.guler@outlook.com.tr>
2021-12-19 23:34:57 +0300

    Tüm movieleri dönen metot yazıldı.

---
+++ DigiturkTest.API/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DigiturkTest.Service.Abstract;
+
+namespace DigiturkTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieManager _movieManager;
+        public MoviesController(IMovieManager movieManager)
+        {
+            _movieManager = movieManager;
+        }
+        [HttpGet("GetAllMovies")]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var movies = _movieManager.GetAllAsync();
+            return Ok(movies);
+        }
+    }
+}
